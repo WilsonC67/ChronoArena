4ee1f848a2a6de6fb5e9f183fc5b437b10b3fc44
BrodyChev86 <brodychev86@gmail.com>
2026-03-26 13:47:03 -0400

    Changed the GameClient to no longer send UDP since that is handled in the ChronoArenaClient
    C
    :wq
    
    ESC
    :wq

---
+++ src/GameClient.java
@@ -0,0 +1,135 @@
+import java.awt.event.KeyEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import javax.swing.SwingUtilities;
+
+
+public class GameClient {
+    private static final int UDP_PORT = PropertyFileReader.getUDPPort();
+    private static final int TCP_PORT = PropertyFileReader.getTCPPort();
+    private static final String SERVER_IP = PropertyFileReader.getIP();
+    private DataOutputStream dataOutputStream;
+    private DataInputStream dataInputStream;
+    private byte[] outgoingData = new byte[1024];
+    private Socket socket;
+    private DatagramSocket datagramSocket;
+
+    public GameClient(Socket socket, DatagramSocket datagramSocket){
+        try {
+            this.socket = socket;
+            this.datagramSocket = datagramSocket;
+            this.dataInputStream = new DataInputStream(socket.getInputStream());
+            this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        } catch (Exception e) {
+            System.out.println("ERROR INITIALIZING CLIENT");
+            e.printStackTrace();
+        }
+    }
+
+    //sends a UDP message to the server
+    public void sendUDP(String message, InetAddress IP, int port) throws IOException{
+        outgoingData = message.getBytes();
+        DatagramPacket outgoingPacket = new DatagramPacket(outgoingData, outgoingData.length, IP, port);
+        datagramSocket.send(outgoingPacket);
+    }
+
+    public void recieveTCPMessages(){
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
+                String msgFromServer;
+
+                try {
+                    while(socket.isConnected()){
+                    msgFromServer = dataInputStream.readUTF();
+
+                    //used for testing what TCP messages are recieved
+                    //can delete later
+                    System.out.println(msgFromServer);
+                }
+                } catch (IOException e) {
+                    System.out.println("ERROR IN RECIEVING TCP MESSAGE FROM SERVER");
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    //reads input from the ChronoArenaClient class and sends a UDP message with their key input to the server
+    public void sendPressed(int key){
+        switch (key) {
+            case KeyEvent.VK_W:
+                System.out.println("W");
+
+                try {
+                    InetAddress address = InetAddress.getByName(SERVER_IP);
+                    sendUDP("W", address, UDP_PORT);
+                } catch (Exception e) {
+                }
+
+                break;
+
+            case KeyEvent.VK_S:
+                System.out.println("S");
+                try {
+                    InetAddress address = InetAddress.getByName(SERVER_IP);
+                    sendUDP("S", address, UDP_PORT);
+                } catch (Exception e) {
+                }
+
+                break;
+
+            case KeyEvent.VK_A:
+                System.out.println("A");
+                try {
+                    InetAddress address = InetAddress.getByName(SERVER_IP);
+                    sendUDP("A", address, UDP_PORT);
+                } catch (Exception e) {
+                }
+
+                break;
+            case KeyEvent.VK_D:
+                System.out.println("D");
+                try {
+                    InetAddress address = InetAddress.getByName(SERVER_IP);
+                    sendUDP("D", address, UDP_PORT);
+                } catch (Exception e) {
+                }
+
+                break;
+            case KeyEvent.VK_SPACE:
+                System.out.println("SPACE");
+                try {
+                    InetAddress address = InetAddress.getByName(SERVER_IP);
+                    sendUDP("SPACE", address, UDP_PORT);
+                } catch (Exception e) {
+                }
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket(SERVER_IP, TCP_PORT);
+            DatagramSocket datagramSocket = new DatagramSocket();
+        
+            GameClient gameClient = new GameClient(socket, datagramSocket);
+            
+            //sends a message to the server that a new player has connected
+            gameClient.dataOutputStream.writeUTF("PLAYER_CONNECT");
+            gameClient.dataOutputStream.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
